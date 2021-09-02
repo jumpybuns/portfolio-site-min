195235a81ef1ae291a76a4aca48fcbea1e0e9750
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-09-02 13:09:06 -0700

    add links

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import EmailIcon from "@material-ui/icons/Email";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
@@ -5,9 +6,21 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 const Footer = () => {
   return (
     <footer>
-      <GitHubIcon />
-      <EmailIcon />
-      <LinkedInIcon />
+      <Link href={"https://github.com/jumpybuns"} passHref>
+        <a target="_blank">
+          <GitHubIcon />
+        </a>
+      </Link>
+      <Link href={"mailto:ethanpiercepresents@gmail.com"} passHref>
+        <a target="_blank">
+          <EmailIcon />
+        </a>
+      </Link>
+      <Link href={"https://www.linkedin.com/in/ethanpiercepresents/"} passHref>
+        <a target="_blank">
+          <LinkedInIcon />
+        </a>
+      </Link>
       <p>Copyright 2021 Ethan Pierce</p>
     </footer>
   );
