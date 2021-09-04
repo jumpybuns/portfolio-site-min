eee79e4373968abcfd8d932ad35c6df4d9b425bf
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-09-03 18:51:12 -0700

    add contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Head from "next/head";
 import styles from "../styles/Contact.module.css";
+import Circles from "../svg/staticCircles";
+import Link from "next/link";
 
 const Contact = () => {
   return (
@@ -9,21 +11,67 @@ const Contact = () => {
         <title>Ethan Pierce | Contact</title>
       </Head>
       <div id="contact" className={styles.container}>
+        <Circles />
         <div className={styles.contactMe}>
           <h1>Contact Me</h1>
         </div>
-        <ul className={styles.links}>
-          <li>EthanPiercePresents@gmail.com</li>
-          <li>GitHub</li>
-          <li>LinkedIn</li>
+        <div className={styles.text}>
+          <h3>
+            Drop me line! I love to chat about projects, philosphy, games,
+            music, art, movies...everything!{" "}
+          </h3>
+        </div>
+        <ul className={styles.socials}>
+          <h2>Socials</h2>
+
+          <li>
+            <Link
+              href={"https://www.linkedin.com/in/ethanpiercepresents/"}
+              passHref
+            >
+              <a target="_blank">LinkedIn</a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href={"https://www.instagram.com/eeebypristine/"} passHref>
+              <a target="_blank">Instagram</a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href={"https://github.com/jumpybuns"} passHref>
+              <a target="_blank">GitHub</a>
+            </Link>
+          </li>
         </ul>
-        <div className={styles.downloadButton}>
+        <ul className={styles.contacts}>
+          <h2>Contacts</h2>
+          <li>
+            <Link href={"mailto:ethanpiercepresents@gmail.com"} passHref>
+              <a target="_blank">Email</a>
+            </Link>
+          </li>
+          <li>541.999.9714</li>
+          <li>
+            <Link
+              href={
+                "https://api.whatsapp.com/send/?phone=%2B15419999714&text=Hi%21&app_absent=0"
+              }
+              passHref
+            >
+              <a target="_blank">WhatsApp</a>
+            </Link>
+          </li>
+        </ul>
+        <div className={styles.resume}>
           <a
-            className={styles.myButton}
+            target="_blank"
+            rel="noreferrer"
             href={"https://i.postimg.cc/jdC9sY2q/Ethan-Pierce-Resume.png"}
             download="Ethan Pierce Resume"
           >
-            Download Resume
+            Resume
           </a>
         </div>
       </div>
