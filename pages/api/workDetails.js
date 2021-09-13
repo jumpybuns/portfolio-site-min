72563c020668f04640629a05aac937579b6e07ce
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-09-13 12:36:42 -0700

    change animations

---
+++ pages/api/workDetails.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    auroraTitle: "Aurora Synth",
+    auroraCode: "https://github.com/Karaoke-Roulette",
+    auroraText:
+      "Elegant interactive web browser synth built with vanilla JavaScript and Web Audio API. I implimented the series of audio nodes and created the different wave shapes and filters as well as used ChartJS to create the dynamic graph",
+  },
+  {
+    karaokeTitle: "Karaoke Roulette",
+    karaokeCode: "https://github.com/Karaoke-Roulette",
+    karaokeText:
+      "Social app utilizing YouTube API to create randomized karaoke videos. We used Javascript, React, SQL, Heroku, Netlify, CSS I created the API call, implemented the authorization, ran tests, and created the CRUD routes.",
+  },
+  {
+    chatableTitle: "Chatable",
+    chatableCode: "https://github.com/Chatable-2021",
+    chatableText:
+      "Instantaneous live chat app and chat rooms created with Socket.IO and Material-UI. I designed and created the messaging emits and broadcasts. I also wrote the User and Auth models/routes.",
+  },
+  {
+    bloomTitle: "Bloom Financial",
+    bloomCode: "https://github.com/Bloom-Financial",
+    bloomText:
+      "Stock Trading learning tool using NewsAPI, Live Stock Ticker, Finance Widget and seamlessly integrating Alpaca Finance with your own portfolio. I setup the Oauth, created the landing page and built the entirety of the Alpaca integration.",
+  },
+];
+
+export default projects;
